ad4088f3ef6b5ce9cea5af13a3b49226a8137232
David Goldberg <offirgold@gmail.com>
2019-12-30 20:41:26 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { data: "" };
+    }
+
+    componentDidMount() {
+    axios
+        .get("http://localhost:8080/musical_pursuit_backend/rest/pursuit")
+        .then((response) =>  {
+            this.setState({data: JSON.stringify(response.data)});
+            console.log(response.data)
+        })
+        .catch(error => console.log(error));
+  }
+
+  render() {
+      if(this.state.data === ''){
+          return <div>Loading</div>;
+      }
+      else {
+          return <div>{this.state.data}</div>
+      }
+    }
 }
 
 export default App;
